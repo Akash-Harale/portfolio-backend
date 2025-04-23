350f7c27ecb749b429ea813836ddcce98f454f52
Akash <akash.v.harale@gmail.com>
2025-04-23 22:36:27 +0530

    chnaged environment variable names

--- db.js
+++ db.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const MONGO_URL = process.env.MONGO_URL;
 
 const connectToDatabase = () => {
   mongoose
-    .connect(process.env.MONGO_URI)
+    .connect(MONGO_URL)
     .then(() => console.log("MongoDB connected"))
     .catch((err) => console.error("MongoDB connection error:", err));
 };
