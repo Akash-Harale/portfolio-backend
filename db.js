6daef9f7b2c83c84446798ab149ca4be102b2bec
Akash <akash.v.harale@gmail.com>
2025-04-25 02:31:51 +0530

    refactored

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const mongodbUrl =
-  "mongodb+srv://akash:harale@cluster0.01kxxgp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  "mongodb+srv://akash:harale@cluster0.01kxxgp.mongodb.net/portfolio?retryWrites=true&w=majority&appName=Cluster0";
 
 const connectToDatabase = mongoose.connect(mongodbUrl);
 
--- routes/recruiter.routes.js
+++ routes/recruiter.routes.js
@@ -1,22 +1,23 @@
 const express = require("express");
+const { RecruiterModel } = require("../models/recruiter.model");
 
 const recruiterRouter = express.Router();
 
-recruiterRouter.post("/add", (req, res) => {
+recruiterRouter.post("/add", async (req, res) => {
   const { name, email, subject, message } = req.body;
 
   if (!name || !email || !subject || !message) {
     return res.status(400).json({ error: "All fields are required" });
   }
 
-  const newRecruiter = {
-    name,
-    email,
-    subject,
-    message,
-  };
-
-  res.status(201).send(newRecruiter);
+  try {
+    const recruiter = new RecruiterModel(req.body);
+    await recruiter.save();
+    res.status(201).json({ message: "Recruiter added successfully" });
+  } catch (error) {
+    console.error("Error adding recruiter:", error);
+    res.status(500).send({ error: "Internal server error" });
+  }
 });
 
 module.exports = { recruiterRouter };
